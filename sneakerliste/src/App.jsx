edaa16169db0968ff574f21c92c9fbf36969ce69
kyri83 <kyromin@gmail.com>
2024-12-14 17:06:39 +0100

    update App.jsx

--- sneakerliste/src/App.jsx
+++ sneakerliste/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css';
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Sneakerlist from './components/Sneakerlist';
 
-function App() {
-  const [count, setCount] = useState(0)
+const supabase = createClient('https://htejmndffmhylxrkdava.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0ZWptbmRmZm1oeWx4cmtkYXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwODQyNTcsImV4cCI6MjA0OTY2MDI1N30.KTraCyrRtWiBT09CNCgAsVd3xelLX8IbLdSejnDPYNE');
+
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Redirect to Dashboard if logged in */}
+        <Route path="/" element={session ? <Navigate to="/dashboard" /> : <Login supabase={supabase} />} />
+        <Route path="/dashboard" element={session ? <Dashboard supabase={supabase} /> : <Navigate to="/" />} />
+        <Route path="/sneakerlist" element={session ? <Sneakerlist /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
